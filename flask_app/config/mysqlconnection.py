9e8700ffa01157007b90e000aab671db23fdec96
Aaron <aaroncourt09@gmail.com>
2022-05-31 15:36:44 -0700

    update to database and offset an image

--- flask_app/config/mysqlconnection.py
+++ flask_app/config/mysqlconnection.py
@@ -5,7 +5,7 @@ class MySQLConnection:
         connection = pymysql.connect(
             host = 'localhost',
             user = 'root', 
-            password = 'root', 
+            password = 'velvetvanity', 
             db = db,
             charset = 'utf8mb4',
             cursorclass = pymysql.cursors.DictCursor,
