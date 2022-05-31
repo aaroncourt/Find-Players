04435916537286af211575cfc8dfb035d63db7fd
Aaron <aaroncourt09@gmail.com>
2022-05-31 14:27:46 -0700

    Revert "updated files for deployment"
    This reverts commit a63bec2099890c2ce558c014a29c96b5452801fb.

--- flask_app/config/mysqlconnection.py
+++ flask_app/config/mysqlconnection.py
@@ -1,12 +1,11 @@
 import pymysql.cursors
 
-
 class MySQLConnection:
     def __init__(self, db):
         connection = pymysql.connect(
             host = 'localhost',
             user = 'root', 
-            password = 'velvetvanity', 
+            password = 'root', 
             db = db,
             charset = 'utf8mb4',
             cursorclass = pymysql.cursors.DictCursor,
