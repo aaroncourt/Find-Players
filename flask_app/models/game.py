18e35449ed5ec91823c053d304f0225153c11632
Aaron <aaroncourt09@gmail.com>
2022-06-01 12:24:28 -0700

    API feature debugging

--- flask_app/models/game.py
+++ flask_app/models/game.py
@@ -12,7 +12,7 @@ class Game:
 
     @classmethod
     def get_game_info(cls, data):
-        results = requests.get(f'https://api.boardgameatlas.com/api/search?ids={data["atlas_game_id"]}&client_id={os.environ.get("boardgame_atlas_api")}')
+        results = requests.get(f'https://api.boardgameatlas.com/api/search?client_id={os.environ.get("boardgame_atlas_api")}&ids={data["atlas_game_id"]}')
         results_json = results.json()
         game_info = results_json["games"][0]
         
@@ -20,7 +20,7 @@ class Game:
 
     @classmethod
     def get_random_game(cls):
-        results = requests.get(f'https://api.boardgameatlas.com/api/search?random=true&client_id={os.environ.get("boardgame_atlas_api")}')
+        results = requests.get(f'https://api.boardgameatlas.com/api/search?client_id={os.environ.get("boardgame_atlas_api")}&random=true')
         results_json = results.json()
         game_info = results_json["games"][0]
 
